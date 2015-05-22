d85cf55324d4e1fd04ef887fcff5d70a2940d9e7
gaetan-pc <gaetan.duhamel@plante-et-cite.fr>
2015-05-22 15:16:01 +0200

    Converting Array comlumns to string

--- db/migrate/20150522090312_alter_array_columns.rb
+++ db/migrate/20150522090312_alter_array_columns.rb
@@ -1,4 +1,36 @@
 class AlterArrayColumns < ActiveRecord::Migration
-  def change
+  def self.up
+    change_column :releves, :fauche, :string
+    change_column :releves, :fauche_periode, :string
+    change_column :releves, :fauche_freq, :string
+    change_column :releves, :paturage, :string
+    change_column :releves, :traitement, :string
+    change_column :releves, :pression, :string
+    change_column :sites, :gen_obj, :string
+    change_column :sites, :hist_occsol, :string
+    change_column :sites, :hist_trav, :string
+    change_column :sites, :cult_amend, :string
+    change_column :sites, :cult_amend_freq, :string
+    change_column :sites, :cult_trav, :string
+    change_column :sites, :cult_trav_freq, :string
+    change_column :sites, :cult_trav_freqinfo, :string
+  end
+
+
+  def self.down
+    change_column :releves, :fauche, :string, array: :true, default: '[]'
+    change_column :releves, :fauche_periode, :string, array: :true, default: '[]'
+    change_column :releves, :fauche_freq, :string, array: :true, default: '[]'
+    change_column :releves, :paturage, :string, array: :true, default: '[]'
+    change_column :releves, :traitement, :string, array: :true, default: '[]'
+    change_column :releves, :pression, :string, array: :true, default: '[]'
+    change_column :sites, :gen_obj, :string, array: :true, default: '[]'
+    change_column :sites, :hist_occsol, :string, array: :true, default: '[]'
+    change_column :sites, :hist_trav, :string, array: :true, default: '[]'
+    change_column :sites, :cult_amend, :string, array: :true, default: '[]'
+    change_column :sites, :cult_amend_freq, :string, array: :true, default: '[]'
+    change_column :sites, :cult_trav, :string, array: :true, default: '[]'
+    change_column :sites, :cult_trav_freq, :string, array: :true, default: '[]'
+    change_column :sites, :cult_trav_freqinfo, :string, array: :true, default: '[]'
   end
 end
