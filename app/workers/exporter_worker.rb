940b864e21b0db080aef15e0c7aa3c21e3715d16
Gaëtan <gaetan.duhamel@plante-et-cite.fr>
2020-04-04 01:23:02 +0200

    Set worker for AdminDataExporterService. Resolves #10

--- app/workers/exporter_worker.rb
+++ app/workers/exporter_worker.rb
@@ -1,7 +1,10 @@
 class ExporterWorker
   include Sidekiq::Worker
+  sidekiq_options retry: false
 
-  def perform(name, count)
-    # ...
+  def perform(user_id)
+    @export = AdminDataExporterService.new(user_id)
+    @export.call
+    AdminMailer.export_process_complete(@export.user, @export.attachment).deliver_now
   end
 end
